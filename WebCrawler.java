511bf701a6fb6e73f3ca759e963bf8b9be8d6322
sujathasomepalli-834 <77578141+sujathasomepalli-834@users.noreply.github.com>
2021-01-18 14:21:41 +0800

    Add files via upload

---
+++ WebCrawler.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+        import java.util.HashSet;
+
+public class WebCrawler {
+    private static final int MAX_DEPTH = 2;
+    private HashSet<String> links;
+
+    public WebCrawler() {
+        links = new HashSet<>();
+    }
+
+    public void getPageLinks(String URL, int depth) {
+        if ((!links.contains(URL) && (depth < MAX_DEPTH))) {
+            System.out.println(">> Depth: " + depth + " [" + URL + "]");
+            try {
+                links.add(URL);
+
+                Document document = Jsoup.connect(URL).get();
+                Elements linksOnPage = document.select("a[href]");
+
+                depth++;
+                for (Element page : linksOnPage) {
+                    getPageLinks(page.attr("abs:href"), depth);
+                }
+            } catch (IOException e) {
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new WebCrawler().getPageLinks("http://www.google.com/", 0);
+    }
+}
